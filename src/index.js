8cf3867c4e27b8a8e32495a7cf9e4a2e0cf0eef4
Sarique <sarique20mh@gmail.com>
2024-10-15 21:16:46 +0530

    Added basic node express functionalities

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { ServerConfig} = require('./config');
+
+const apiRoutes = require('./routes');
+
+const app = express();
+
+
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Successfully started the server on the PORT : ${ServerConfig.PORT}`);
+})
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router(); 
+
+const v1route = require('./v1');
+
+router.use('/v1', v1route);
+
+module.exports = router;
